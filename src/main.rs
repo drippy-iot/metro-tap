a5fb8feccf388552d457f46b0c5cb51925d67511
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-05-22 21:34:07 +0800

    refactor: fully commit to asynchronous operations

--- src/main.rs
+++ src/main.rs
@@ -1,105 +1,63 @@
-mod button;
-mod flow;
-mod http;
-mod snapshot;
-
-use embedded_svc::{
-    http::client::asynch::{Client as HttpClient, TrivialUnblockingConnection},
-    ipv4::IpInfo,
-    utils::asyncify::Asyncify,
-    wifi::{AccessPointInfo, ClientConfiguration, Configuration},
-};
-use esp_idf_hal::{
-    gpio::{PinDriver, Pins, Pull},
-    peripherals::Peripherals,
-    task::executor::EspExecutor,
-};
+use embedded_svc::wifi;
+use esp_idf_hal::{peripherals::Peripherals, task::executor::EspExecutor};
 use esp_idf_svc::{
-    eventloop::EspSystemEventLoop, http::client::EspHttpConnection, nvs::EspDefaultNvsPartition,
-    timer::EspTimerService, wifi::EspWifi,
+    eventloop::EspSystemEventLoop,
+    nvs::EspDefaultNvsPartition,
+    timer::EspTimerService,
+    wifi::{AsyncWifi, EspWifi},
 };
 use esp_idf_sys::{self as _, EspError};
 
-fn main() -> anyhow::Result<()> {
+fn main() -> Result<(), EspError> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_sys::link_patches();
 
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    // Get the peripherals from the board
-    let Peripherals {
-        modem,
-        pins:
-            Pins {
-                gpio18: tap_pin,
-                gpio19: metro_valve_pin,
-                gpio21: tap_valve_pin,
-                gpio22: bypass_pin,
-                gpio23: flow_sensor_pin,
-                ..
-            },
-        ..
-    } = Peripherals::take().ok_or(EspError::from_infallible::<-1>())?;
-
-    // Set up the input pins
-    let flow_sensor = PinDriver::input(flow_sensor_pin)?;
-    let mut tap = PinDriver::input(tap_pin)?;
-    tap.set_pull(Pull::Up)?;
-    let mut bypass = PinDriver::input(bypass_pin)?;
-    bypass.set_pull(Pull::Up)?;
-
-    // Set up the ouput pins
-    let metro_valve = PinDriver::output(metro_valve_pin)?;
-    let tap_valve = PinDriver::output(tap_valve_pin)?;
-
-    let sys_loop = EspSystemEventLoop::take()?;
+    let sysloop = EspSystemEventLoop::take()?;
     let nvs = EspDefaultNvsPartition::take()?;
-    let mut timer_svc = EspTimerService::new()?.into_async();
-    let tap_timer = timer_svc.timer()?;
-    let snapshot_timer = timer_svc.timer()?;
-    drop(timer_svc);
+    let timer_svc = EspTimerService::new()?;
 
-    let mut wifi = EspWifi::new(modem, sys_loop, Some(nvs))?;
+    let Peripherals { modem, .. } = Peripherals::take().ok_or(EspError::from_infallible::<-1>())?;
+    let wifi = EspWifi::new(modem, sysloop.clone(), Some(nvs))?;
+    let mut wifi = AsyncWifi::wrap(wifi, sysloop, timer_svc)?;
+    wifi.set_configuration(&wifi::Configuration::Client(Default::default()))?;
 
-    log::info!("Setting Wi-Fi default configuration...");
-    wifi.set_configuration(&Configuration::Client(ClientConfiguration::default()))?;
+    let rt = EspExecutor::<16, _>::new();
+    rt.spawn_local(async {
+        wifi.start().await?;
+        log::info!("Wi-Fi started");
 
-    log::info!("Starting Wi-Fi...");
-    wifi.start()?;
-
-    'connect: loop {
-        log::info!("Starting a new round of scanning...");
-        for AccessPointInfo { ssid, signal_strength, auth_method, .. } in wifi.scan()? {
+        for wifi::AccessPointInfo { ssid, signal_strength, auth_method, .. } in wifi.scan().await? {
             let Some(name) = ssid.strip_prefix("DRIPPY_") else {
-                log::warn!("Skipping {ssid} [{signal_strength}]...");
+                log::warn!("skipping {ssid} [{signal_strength}]");
                 continue;
             };
 
-            log::info!("Attempting to connect to {name} [{signal_strength}]...");
-            wifi.set_configuration(&Configuration::Client(ClientConfiguration {
+            log::info!("connecting to {name}");
+            wifi.set_configuration(&wifi::Configuration::Client(wifi::ClientConfiguration {
                 ssid,
                 auth_method,
                 password: "drippy-test".into(),
                 ..Default::default()
             }))?;
-            wifi.connect()?;
-            break 'connect;
+            wifi.connect().await?;
+            log::info!("successfully connected to network");
+
+            wifi.wait_netif_up().await?;
+            break;
         }
-    }
 
-    let IpInfo { ip, subnet, .. } = wifi.ap_netif().get_ip_info()?;
-    log::info!("Now connected as {ip} in subnet {subnet}.");
+        let netif = wifi.wifi().sta_netif();
+        let info = netif.get_ip_info()?;
+        log::info!("connected to interface {info:?}");
 
-    let http = EspHttpConnection::new(&Default::default())?;
-    let http = TrivialUnblockingConnection::new(http);
-    let http = HttpClient::wrap(http);
-
-    let exec = EspExecutor::<16, _>::new();
-    exec.spawn(flow::detect(flow_sensor))?.detach();
-    exec.spawn(button::tap::toggle(tap_timer, tap))?.detach();
-    exec.spawn_local(snapshot::tick(snapshot_timer, http))?.detach();
-    exec.run(|| true);
+        Ok::<_, EspError>(())
+    })
+    .unwrap()
+    .detach();
+    rt.run(|| true);
 
     Ok(())
 }

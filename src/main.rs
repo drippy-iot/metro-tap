9122d81215d7401b90aed20054edbb1f73c9b6d2
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-06-14 17:02:06 +0800

    fix: set the global certificate store

--- src/main.rs
+++ src/main.rs
@@ -78,10 +78,11 @@ fn main() -> Result<(), EspError> {
     let timer = timer_svc.timer()?;
 
     // Set up asynchronous HTTP service
-    let cert = X509::pem_until_nul(include_bytes!("_.up.railway.app.crt"));
+    let cert = include_bytes!("_.up.railway.app.crt");
+    unsafe { esp_idf_sys::esp_tls_set_global_ca_store(cert.as_ptr().cast(), cert.len() as u32) };
     let conn = EspHttpConnection::new(&HttpConfig {
-        client_certificate: Some(cert),
         follow_redirects_policy: FollowRedirectsPolicy::FollowAll,
+        use_global_ca_store: true,
         ..Default::default()
     })?;
     let conn = TrivialUnblockingConnection::new(conn);
